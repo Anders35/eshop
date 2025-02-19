04e1fee90f648054dd11b237ae7d387ff75e3444
Anders35 <andersleo05@gmail.com>
2025-02-20 01:50:52 +0700

    fix: remove empty method

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -17,9 +17,7 @@ class ProductRepositoryTest {
 
     @InjectMocks
     ProductRepository productRepository;
-    @BeforeEach
-    void setUp() {
-    }
+
     @Test
     void testCreateAndFind() {
         Product product = new Product();
