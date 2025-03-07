e385fa82dadcfb53149629342f5eb496f81ef30a
Anders35 <andersleo05@gmail.com>
2025-03-07 20:28:49 +0700

    [GREEN] implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -2,12 +2,39 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PaymentRepository {
-    public Payment save(Payment payment) {return null;}
-    public Payment findById(String id) {return null;}
-    public List<Payment> findAll() {return new ArrayList<>();}
-    public void delete(String id) {}
-    public List<Payment> findByOrderId(String orderId) {return new ArrayList<>();}
+
+    private Map<String, Payment> paymentMap;
+
+    public PaymentRepository() {
+        this.paymentMap = new HashMap<>();
+    }
+
+    public Payment save(Payment payment) {
+        paymentMap.put(payment.getId(), payment);
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        return paymentMap.get(id);
+    }
+
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentMap.values());
+    }
+
+    public void delete(String id) {
+        paymentMap.remove(id);
+    }
+
+    public List<Payment> findByOrderId(String orderId) {
+        return paymentMap.values().stream()
+                .filter(payment -> payment.getOrder().getId().equals(orderId))
+                .collect(Collectors.toList());
+    }
 }
